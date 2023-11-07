493921d139b473ec3a6a294cf7d6910bb38aa073
saeki <0ec6ky47385587v@gmail.com>
2023-11-07 16:55:44 +0900

    設定

--- app.js
+++ app.js
@@ -23,24 +23,15 @@ const con = mysql.createConnection({
 
 app.use(express.static("assets"));
 
-app.get("/", (req, res) => {
-  const sql = "select * from clients";
-  con.query(sql, function (err, result, fields) {
-    if (err) throw err;
-    res.render("index", {
-      users: result,
-    });
-  });
-});
-
 app.post("/", (req, res) => {
-  const sql = "INSERT INTO clients SET ?";
+  const sql = "INSERT INTO questionnaire SET ?";
   con.query(sql, req.body, function (err, result, fields) {
     if (err) throw err;
     console.log(result);
     res.redirect("/");
   });
 });
+
 
 app.get("/create", (req, res) => {
   res.sendFile(path.join(__dirname, "html/form.html"));
@@ -53,25 +44,6 @@ app.get("/edit/:id", (req, res) => {
     res.render("edit", {
       user: result,
     });
-  });
-});
-
-app.post("/update/:id", (req, res) => {
-  console.log(req.params.id);
-  const sql = "UPDATE clients SET ? WHERE id = " + req.params.id;
-  con.query(sql, req.body, function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-    res.redirect("/");
-  });
-});
-
-app.get("/delete/:id", (req, res) => {
-  const sql = "DELETE FROM clients WHERE id = ?";
-  con.query(sql, [req.params.id], function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-    res.redirect("/");
   });
 });
 
